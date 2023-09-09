8e111802874490d1c708b34bd326ad6cd6e5c6ac
yurill <rawityurill@gmail.com>
2023-09-09 21:26:33 +0700

    Make a deck selecting before starts into playing + editing script

--- frontend/script.js
+++ frontend/script.js
@@ -1,10 +1,22 @@
-let summitButton = document.getElementById("main__user-input--inputbutton");
-let TemporaryInput = document.getElementById("main__user-input--inputbar");
-let MainDisplay = document.getElementById("main__display-screen");
-
 
 // Test to change main display text when summitButton is clicked
+let summitButton = document.getElementById("main__user-input--inputbutton");
 summitButton.addEventListener("click", function(){
-    const TemporaryInputText = TemporaryInput.value;
+    let TemporaryInput = document.getElementById("main__user-input--inputbar");
+    let MainDisplay = document.querySelector("#main__display--cardplay span");
+
+    let TemporaryInputText = TemporaryInput.value;
     MainDisplay.innerHTML = TemporaryInputText;
+    TemporaryInput.value = ""; 
 });
+
+
+//Switching state of display between before and after choosing a deck to play and playing WORK IS PROGRESS
+let chooseDeckButton = document.getElementById("main__display--buttonselect-deck");
+chooseDeckButton.addEventListener("click", ()=>{
+    let DeckSelectingScreen = document.getElementById("main__display--deckchooser");
+    let DeckPlayingScreen = document.getElementById("main__display--screen");
+    DeckSelectingScreen.style.display = "none";
+    DeckPlayingScreen.style.display = "block";
+
+})
