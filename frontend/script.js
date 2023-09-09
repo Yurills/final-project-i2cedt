ac59d175863bbb757f5be2cd0f56dea931739790
Thamvarut Wannachetisara <66747259+RealThamvarut@users.noreply.github.com>
2023-09-09 21:48:00 +0700

    Reset Display incase there is no Input

--- frontend/script.js
+++ frontend/script.js
@@ -1,19 +1,21 @@
 
 // Test to change main display text when summitButton is clicked
 let summitButton = document.getElementById("main__user-input--inputbutton");
-summitButton.addEventListener("click", function(){
+summitButton.addEventListener("click", () => {
     let TemporaryInput = document.getElementById("main__user-input--inputbar");
     let MainDisplay = document.querySelector("#main__display--cardplay span");
-
     let TemporaryInputText = TemporaryInput.value;
+
     MainDisplay.innerHTML = TemporaryInputText;
-    TemporaryInput.value = ""; 
+    // RESET DISPLAY Just in case there is NO INPUT
+    if (TemporaryInputText == "") MainDisplay.innerHTML = "text here";
+    TemporaryInput.value = "";
 });
 
 
 //Switching state of display between before and after choosing a deck to play and playing WORK IS PROGRESS
 let chooseDeckButton = document.getElementById("main__display--buttonselect-deck");
-chooseDeckButton.addEventListener("click", ()=>{
+chooseDeckButton.addEventListener("click", () => {
     let DeckSelectingScreen = document.getElementById("main__display--deckchooser");
     let DeckPlayingScreen = document.getElementById("main__display--screen");
     DeckSelectingScreen.style.display = "none";
