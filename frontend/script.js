0b065fd8b593c1e036e3924e993e7c411d3f5985
Yurills <139625660+Yurills@users.noreply.github.com>
2023-10-04 21:02:20 +0700

    start button switch display states. minor code clean up

--- frontend/script.js
+++ frontend/script.js
@@ -1,7 +1,10 @@
-// button selection in main display
+
+//generate button with deck name
 let button_MainDisplay = document.getElementsByClassName("deckname-button");
 
-let button_Selection_Value;
+// button selection in main display
+
+let button_Selection_Value = button_MainDisplay[0].value;
 let button_Selection = (event)=>{
     for (let i=0;i<button_MainDisplay.length;i++) {
         button_MainDisplay[i].style.backgroundColor = "#8898E0";
@@ -12,8 +15,20 @@ let button_Selection = (event)=>{
     document.getElementById(button_Selection_Value).style.backgroundColor = "#ffd683";
     document.getElementById(button_Selection_Value).style.borderColor = "#ae8454";
 }
-
 for (let i=0;i<button_MainDisplay.length;i++) {
     button_MainDisplay[i].addEventListener('click', button_Selection);   
 }
+
+
+
+// switching to flashcard display
+let switchDisplay_Start = ()=> {
+    document.getElementById("Main-Display").style.display = "none";
+    document.getElementById("Flashcard").style.display = "block";
+}
+
+
+let button_MainDisplay__START = document.getElementById("Main-Display__Start-Button");
+button_MainDisplay__START.addEventListener('click', switchDisplay_Start);
+
 
