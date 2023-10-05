233deae10923cf7526213779ae5d8cba938235d4
Nithi P <67043780+MIRaGE-NP@users.noreply.github.com>
2023-10-06 01:06:30 +0700

    Add "Edit" page

--- frontend/scripts/script.js
+++ frontend/scripts/script.js
@@ -5,32 +5,48 @@ let button_MainDisplay = document.getElementsByClassName("deckname-button");
 // button selection in main display
 
 let button_Selection_Value = button_MainDisplay[0].value;
-let button_Selection = (event)=>{
-    for (let i=0;i<button_MainDisplay.length;i++) {
+let button_Selection = (event) => {
+    for (let i = 0; i < button_MainDisplay.length; i++) {
         button_MainDisplay[i].style.backgroundColor = "#8898E0";
-        button_MainDisplay[i].style.borderColor = "#788be0" 
+        button_MainDisplay[i].style.borderColor = "#788be0"
     }
     button_Selection_Value = event.target.value;
     console.log(button_Selection_Value);
     document.getElementById(button_Selection_Value).style.backgroundColor = "#ffd683";
     document.getElementById(button_Selection_Value).style.borderColor = "#ae8454";
 }
-for (let i=0;i<button_MainDisplay.length;i++) {
-    button_MainDisplay[i].addEventListener('click', button_Selection);   
+for (let i = 0; i < button_MainDisplay.length; i++) {
+    button_MainDisplay[i].addEventListener('click', button_Selection);
 }
 //}
 
 //export function switchDisplay(){
 // switching to flashcard display
-let switchDisplay_Start = ()=> {
+let switchDisplay_Start = () => {
     document.getElementById("Main-Display").style.display = "none";
     document.getElementById("Flashcard").style.display = "block";
 }
 
-let switchDisplay_Return = ()=> {
+let switchDisplay_Return = () => {
     document.getElementById("Main-Display").style.display = "block";
     document.getElementById("Flashcard").style.display = "none";
 }
+
+let switchDisplay_Edit = () => {
+    document.getElementById("Main-Display").style.display = "none";
+    document.getElementById("Edit").style.display = "Block";
+}
+
+let switchDisplay_Edit_Return = () => {
+    document.getElementById("Main-Display").style.display = "block";
+    document.getElementById("Edit").style.display = "none";
+}
+
+let Edit_Return_Button = document.getElementById("Edit-Return-Button");
+Edit_Return_Button.addEventListener('click', switchDisplay_Edit_Return);
+
+let button_MainDisplay__EDIT = document.getElementById("Main-Display__Edit-Button");
+button_MainDisplay__EDIT.addEventListener('click', switchDisplay_Edit);
 
 let Flashcard_Return_Button = document.getElementById("Return-Button");
 Flashcard_Return_Button.addEventListener('click', switchDisplay_Return);
