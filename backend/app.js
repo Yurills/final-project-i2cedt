3a0719264a55664875f94cbdf967371f30e87f71
Nithi .P <p51104.2015@gmail.com>
2023-10-12 11:31:20 +0700

    no message

--- backend/app.js
+++ backend/app.js
@@ -10,8 +10,8 @@ import mongoose from 'mongoose';
 mongoose.connect(process.env.MONGO_URL);
 
 const db = mongoose.connection;
-db.on('error', (error) => {console.error(error)});
-db.once('open', ()=> {console.log("Connect to MONGODB")});
+db.on('error', (error) => { console.error(error) });
+db.once('open', () => { console.log("Connect to MONGODB") });
 
 app.use(express.json());
 app.use(cors());
@@ -19,10 +19,10 @@ app.use(cors());
 import Router from './routes/deckInfo.js';
 import Router2 from './routes/userInfo.js';
 app.use('/deckInfo', Router);
-app.use('/userInfo',Router2)
+app.use('/userInfo', Router2)
 
 app.use(express.static('frontend/public'));
 
-app.listen(PORT, ()=> {
-    console.log (`server live at http://localhost:${PORT}`);
+app.listen(PORT, () => {
+    console.log(`server live at http://localhost:${PORT}`);
 })
